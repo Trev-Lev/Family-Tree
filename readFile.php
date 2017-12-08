987155b6bf2cece65b46ebd23195a9fccf203f4f
Trevor Levins <tnl9t5@mail.missouri.edu>
2017-12-07 19:41:02 -0600

    Shows node ID

--- readFile.php
+++ readFile.php
@@ -1,24 +1,21 @@
 <?php
 require ('db_credentials.php');
-
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 if ($conn->connect_error)
 {
     die("Connection failed: " . $conn->connect_error);
 }
-
-
-
 //echo "Work";
 $sql="SELECT NAME, id, parentID, dob, dod FROM members";
+<<<<<<< HEAD
+=======
 
+>>>>>>> 3d2bc686e68fabbe0477df0b3425c003940daaf8
 $person = array();
 //$text = array();
 $response = array();
 $members = array();
 $result=mysqli_query($conn,$sql);
-
 while($row=mysqli_fetch_array($result))
 {
 $name=$row['NAME'];
@@ -31,33 +28,32 @@ $htmlclass='white';
 echo $id;
 echo $name;
 */
-
 //$person = array('id'=> $id, 'parentid'=>$parentid, 'name'=>$name);
 //$text = array('name' => $name,'dob'=>$dob,'dod'=>$dod);
+<<<<<<< HEAD
+$text = array('name' => $name, 'title' => $id);
+//$text1 = array('text'=>$text);
+$members[] = array('text'=>$text,'id'=>$id,'parentid'=>$parentid,'HTMLclass'=>$htmlclass);
+=======
 $text = array('name' => $name);
 //$text1 = array('text'=>$text);
 
 
 $members[] = array('text'=>$text,'id'=>$id,'parentid'=>$parentid,'HTMLclass'=>$htmlclass);
 
+>>>>>>> 3d2bc686e68fabbe0477df0b3425c003940daaf8
 }
-
 //$response['members'] = $members;
-
 //echo json_encode($response);
 echo json_encode($members);
 //$fp = fopen('members.json', 'w');
 //fwrite($fp, json_encode($response));
 //fclose($fp);
-
-
 /*
 $filename = 'members.json';
 $somecontent = "Add this to the file\n";
-
 // Let's make sure the file exists and is writable first.
 if (is_writable($filename)) {
-
     // In our example we're opening $filename in append mode.
     // The file pointer is at the bottom of the file hence
     // that's where $somecontent will go when we fwrite() it.
@@ -65,20 +61,15 @@ if (is_writable($filename)) {
          echo "Cannot open file ($filename)";
          exit;
     }
-
     // Write $somecontent to our opened file.
     if (fwrite($handle, $somecontent) === FALSE) {
         echo "Cannot write to file ($filename)";
         exit;
     }
-
     echo "Success, wrote ($somecontent) to file ($filename)";
-
     fclose($handle);
-
 } else {
     echo "The file $filename is not writable";
 }
-
 echo "Read to file";*/
 ?>
